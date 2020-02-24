860bab7d52a69b442a96a75f2281bf9e5f14f0f3
ajithvgiri <ajithvgiri@qkopy.com>
2020-02-25 00:11:48 +0530

    created PhotosFragment
    PhotosFragment is the main fragment to list the photos in recyclerview

--- app/src/main/java/com/ajithvgiri/nasa/ui/home/PhotosFragment.kt
+++ app/src/main/java/com/ajithvgiri/nasa/ui/home/PhotosFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.FragmentNavigator
@@ -15,37 +14,26 @@ import com.ajithvgiri.nasa.R
 import com.ajithvgiri.nasa.adapter.ImageGridAdapter
 import kotlinx.android.synthetic.main.fragment_image_grid.*
 
-class ImageGridFragment : Fragment(), OnItemClickListener {
+class PhotosFragment : Fragment() {
 
-    private lateinit var imageGridViewModel: ImageGridViewModel
-    private lateinit var imageGridAdapter: ImageGridAdapter
+    private lateinit var photosViewModel: PhotosViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        imageGridViewModel = ViewModelProvider(this).get(ImageGridViewModel::class.java)
+        photosViewModel = ViewModelProvider(this).get(PhotosViewModel::class.java)
         return inflater.inflate(R.layout.fragment_image_grid, container, false)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        imageGridAdapter = ImageGridAdapter(this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        GridLayoutManager(context, 2).apply {
-            recyclerView.layoutManager = this
-            recyclerView.adapter = imageGridAdapter
-        }
     }
 
-    override fun getItemImage(view: View,position: Int) {
-        val extras = FragmentNavigator.Extras.Builder().addSharedElement(view as TextView, "title_${position}").build()
-        val action = ImageGridFragmentDirections.actionHomeFragmentToHomeSecondFragment("From ImageGridFragment $position")
-        NavHostFragment.findNavController(this@ImageGridFragment).navigate(action,extras)
-    }
 }
