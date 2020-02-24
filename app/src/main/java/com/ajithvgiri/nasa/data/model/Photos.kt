551d2d36e1cc088fa8c1b4fd4a681996b183eb48
ajithvgiri <ajithvgiri@qkopy.com>
2020-02-25 01:17:14 +0530

    created PhotosRepository
    PhotosRepository is the data handling class for parsing image from json in asset folder and bind the values to `listOfPhotosMutableLiveData` which is MutableLiveData this Mutable is binding in PhotosViewModels class

--- app/src/main/java/com/ajithvgiri/nasa/data/model/Photos.kt
+++ app/src/main/java/com/ajithvgiri/nasa/data/model/Photos.kt
@@ -1,14 +1,16 @@
 package com.ajithvgiri.nasa.data.model
 
+import com.squareup.moshi.JsonClass
 import java.io.Serializable
 
+@JsonClass(generateAdapter = true)
 data class Photos(
-    var copyright: String,
-    var date: String,
-    var explanation: String,
-    var hdurl: String,
-    var media_type: String,
-    var service_version: String,
-    var title: String,
-    var url: String
+    var copyright: String?=null,
+    var date: String?=null,
+    var explanation: String?=null,
+    var hdurl: String?=null,
+    var media_type: String?=null,
+    var service_version: String?=null,
+    var title: String?=null,
+    var url: String?=null
 ) : Serializable
--- app/src/main/java/com/ajithvgiri/nasa/data/repository/PhotosRepository.kt
+++ app/src/main/java/com/ajithvgiri/nasa/data/repository/PhotosRepository.kt
@@ -1,2 +1,50 @@
 package com.ajithvgiri.nasa.data.repository
 
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ajithvgiri.nasa.data.model.Photos
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import java.io.IOException
+import java.lang.reflect.Type
+
+class PhotosRepository {
+
+    val listOfPhotosMutableLiveData = MutableLiveData<List<Photos>>()
+    private val moshi = Moshi.Builder().build()
+    private val type: Type = Types.newParameterizedType(List::class.java, Photos::class.java)
+    private val jsonAdapter: JsonAdapter<List<Photos>> = moshi.adapter(type)
+
+    fun getPhotos(context: Context): LiveData<List<Photos>> {
+        getAssetJsonData(context)?.let { json ->
+            val photos: List<Photos>? = jsonAdapter.fromJson(json)
+            photos?.forEach { photo ->
+                Log.d("Photos", "response from photos ${photo.title}")
+            }
+            photos?.let { listOfPhotos ->
+                listOfPhotosMutableLiveData.value = listOfPhotos
+            }
+        }
+        return listOfPhotosMutableLiveData
+    }
+
+    private fun getAssetJsonData(context: Context): String? {
+        val json: String
+        try {
+            val inputStream = context.assets.open("data.json")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.use { it.read(buffer) }
+            json = String(buffer)
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        // print the data
+        Log.i("data", json)
+        return json
+    }
+}
