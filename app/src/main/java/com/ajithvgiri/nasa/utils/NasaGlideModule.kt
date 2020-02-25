4de701bc62c43cdceee55125fb12333f24bb84ef
ajithvgiri <ajithvgiri@qkopy.com>
2020-02-25 12:32:06 +0530

    created GlideModule and GlideExtention Function
    GlideModule used for adding custom Okhttp requesting time because some images requires more than 30 seconds to load so in NasaGlideModule added 60 seconds time.
    
    Glide Extention function Extented from ImagaView object for loading image from `PhotoFragment` and `PhotoPagerAdapter`

--- app/src/main/java/com/ajithvgiri/nasa/utils/NasaGlideModule.kt
+++ app/src/main/java/com/ajithvgiri/nasa/utils/NasaGlideModule.kt
@@ -1,17 +1,28 @@
 package com.ajithvgiri.nasa.utils
 
 import android.content.Context
+
 import android.graphics.Bitmap
+import androidx.annotation.NonNull
+import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.LruResourceCache
+import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.transition.DrawableCrossFadeFactory.Builder
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.bumptech.glide.signature.ObjectKey
+import okhttp3.OkHttpClient
+import java.io.InputStream
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory.Builder
+import java.util.concurrent.TimeUnit
 
 
 @GlideModule
@@ -24,17 +35,29 @@ class NasaGlideModule : AppGlideModule() {
         builder.setDefaultRequestOptions(requestOptions(context))
     }
 
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+        val factory = OkHttpUrlLoader.Factory(client)
+        glide.registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java, factory
+        )
+    }
+
     companion object {
-        var drawableCrossFadeFactory = Builder().setCrossFadeEnabled(true).build()
+
+        var drawableCrossFadeFactory: DrawableCrossFadeFactory = Builder().setCrossFadeEnabled(true).build()
 
         private fun requestOptions(context: Context): RequestOptions {
             return RequestOptions()
                 .signature(ObjectKey(System.currentTimeMillis() / (24 * 60 * 60 * 1000)))
-                .override(200, 200)
-                .centerCrop()
+                .override( Target.SIZE_ORIGINAL)
                 .encodeFormat(Bitmap.CompressFormat.JPEG)
                 .encodeQuality(100)
-                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .format(DecodeFormat.PREFER_ARGB_8888)
                 .skipMemoryCache(false)
         }
